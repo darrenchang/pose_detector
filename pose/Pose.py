6a6f6ca81e6d839ccac2da51dc9c1bf57c7f6144
darrenchang <darrenchang951@gmail.com>
2024-10-07 23:22:10 +0800

    Push pose landmarks data to redislite

--- pose/Pose.py
+++ pose/Pose.py
@@ -1,5 +1,9 @@
+import json
+from time import perf_counter
+
 import cv2
 import mediapipe as mp
+from redislite import Redis
 from World import World
 
 
@@ -15,7 +19,6 @@ class Pose:
             "model_complexity": 0,
         }
         self.pose = mp_pose.Pose(**pose_options)
-        self.cap = cv2.VideoCapture(cam)
 
     def inference(self, im):
         results = self.pose.process(im)
@@ -38,6 +41,7 @@ if __name__ == "__main__":
     pose = Pose()
     world = World()
     cap = cv2.VideoCapture(0)
+    redis_connection = Redis("/tmp/redis.db")
     mp_drawing = mp.solutions.drawing_utils
     mp_drawing_styles = mp.solutions.drawing_styles
 
@@ -45,6 +49,7 @@ if __name__ == "__main__":
         print("Cannot open camera")
         exit()
     while True:
+        s = perf_counter()
         ret, img = cap.read()
         if not ret:
             print("Cannot receive frame")
@@ -52,15 +57,18 @@ if __name__ == "__main__":
         img = cv2.resize(img, (1920, 1080))
         img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results, landmarks = pose.inference(img2)
-        # mp_drawing.draw_landmarks(
-        #     img,
-        #     results.pose_landmarks,
-        #     mp.solutions.pose.POSE_CONNECTIONS,
-        #     landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
-        # )
+        redis_connection.set("landmarks", json.dumps(landmarks))
+        mp_drawing.draw_landmarks(
+            img,
+            results.pose_landmarks,
+            mp.solutions.pose.POSE_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
+        )
         world.draw_pose_landmarks(landmarks)
-        # cv2.imshow("pose", img)
-        # if cv2.waitKey(5) == ord("q"):
-        #     break
+        cv2.imshow("pose", img)
+        redis_connection.get("landmarks")
+        print(f"FPS: {1 / (perf_counter() - s)}")
+        if cv2.waitKey(5) == ord("q"):
+            break
     cap.release()
     cv2.destroyAllWindows()
