6b6556c69b840313464f7ed9c1e8fbb3d9d1ea00
darrenchang <darrenchang951@gmail.com>
2024-10-16 21:43:14 +0800

    Add console message

--- start.sh
+++ start.sh
@@ -4,12 +4,14 @@ set -e
 
 # Set up python venv if not already setup
 if [ ! -d app/venv ]; then
+  echo "Setting up python venv..."
   python3 -m venv app/venv;
   source app/venv/bin/activate;
   python -m pip install app/.;
 fi
 
 (
+echo "Starting the server..."
 cd app;
 gunicorn -c config.py app_main:app;
 )
