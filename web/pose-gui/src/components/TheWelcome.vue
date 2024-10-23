a81b6c695e868d16df8188b1c3ca2e69c8009ea0
darrenchang <darrenchang951@gmail.com>
2024-10-23 23:07:01 +0800

    Add toggle button and menu for the left sider

--- web/pose-gui/src/components/TheWelcome.vue
+++ web/pose-gui/src/components/TheWelcome.vue
@@ -1,25 +1,105 @@
-<script setup lang="ts">
-import { NLayout, NLayoutSider, NLayoutContent } from 'naive-ui'
-</script>
-
 <template>
   <n-layout has-sider>
-    <n-layout-sider
-      content-style="
-        padding: 24px;
-      "
-    >
-      a_pose_name
+    <n-layout-sider bordered collapse-mode="width" :width="240" :native-scrollbar="false" show-trigger>
+      <n-menu :options="menuOptions" />
     </n-layout-sider>
-    <n-layout-content
-      content-style="
+    <n-layout-content content-style="
         padding: 24px;
-      "
-    >
+      ">
       hello
     </n-layout-content>
   </n-layout>
 </template>
+<script setup lang="ts">
+import type { Component } from 'vue'
+import { h } from 'vue'
+import { NLayout, NLayoutSider, NLayoutContent, NMenu } from 'naive-ui'
+import { NIcon } from 'naive-ui'
+import {
+  BookOutline as BookIcon,
+  PersonOutline as PersonIcon,
+  WineOutline as WineIcon,
+} from '@vicons/ionicons5'
+
+function renderIcon(icon: Component) {
+  return () => h(NIcon, null, { default: () => h(icon) })
+}
+
+const menuOptions = [
+  {
+    label: 'Hear the Wind Sing',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon),
+  },
+  {
+    label: 'Pinball 1973',
+    key: 'pinball-1973',
+    icon: renderIcon(BookIcon),
+    disabled: true,
+    children: [
+      {
+        label: 'Rat',
+        key: 'rat',
+      },
+    ],
+  },
+  {
+    label: 'A Wild Sheep Chase',
+    key: 'a-wild-sheep-chase',
+    disabled: true,
+    icon: renderIcon(BookIcon),
+  },
+  {
+    label: 'Dance Dance Dance',
+    key: 'Dance Dance Dance',
+    icon: renderIcon(BookIcon),
+    children: [
+      {
+        type: 'group',
+        label: 'People',
+        key: 'people',
+        children: [
+          {
+            label: 'Narrator',
+            key: 'narrator',
+            icon: renderIcon(PersonIcon),
+          },
+          {
+            label: 'Sheep Man',
+            key: 'sheep-man',
+            icon: renderIcon(PersonIcon),
+          },
+        ],
+      },
+      {
+        label: 'Beverage',
+        key: 'beverage',
+        icon: renderIcon(WineIcon),
+        children: [
+          {
+            label: 'Whisky',
+            key: 'whisky',
+          },
+        ],
+      },
+      {
+        label: 'Food',
+        key: 'food',
+        children: [
+          {
+            label: 'Sandwich',
+            key: 'sandwich',
+          },
+        ],
+      },
+      {
+        label: 'The past increases. The future recedes.',
+        key: 'the-past-increases-the-future-recedes',
+      },
+    ],
+  },
+]
+</script>
 <style scoped>
 .n-layout-sider {
   background: rgba(128, 128, 128, 0.3);
