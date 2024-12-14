b139ff46c19b4116740d33e8284dcd51a5dec8ce
darrenchang <darrenchang951@gmail.com>
2024-12-14 16:43:14 +0800

    Move sidebars to homeviewer route

--- web/pose-gui/src/App.vue
+++ web/pose-gui/src/App.vue
@@ -1,132 +1,17 @@
 <template>
   <n-layout>
     <n-layout-header>Pose</n-layout-header>
-    <n-layout-content>
-      <n-layout has-sider>
-        <n-layout-sider
-          class="n-layout-sider--left-placement"
-          bordered
-          collapse-mode="width"
-          :width="240"
-          :native-scrollbar="false"
-          show-trigger
-        >
-          <n-menu :options="menuOptions" />
-        </n-layout-sider>
-        <n-layout-content>
-          <RouterView />
-        </n-layout-content>
-        <n-layout-sider
-          class="n-layout-sider--right-placement"
-          bordered
-          collapse-mode="width"
-          :width="240"
-          :native-scrollbar="false"
-          show-trigger
-        >
-          <n-menu :options="menuOptions" />
-        </n-layout-sider>
-      </n-layout>
-    </n-layout-content>
+    <RouterView />
   </n-layout>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import type { Component } from 'vue'
-import { NIcon } from 'naive-ui'
-import { h } from 'vue'
 import {
   NLayout,
   NLayoutHeader,
-  NLayoutSider,
-  NLayoutContent,
-  NMenu,
 } from 'naive-ui'
-import {
-  BookOutline as BookIcon,
-  PersonOutline as PersonIcon,
-  WineOutline as WineIcon,
-} from '@vicons/ionicons5'
 
-function renderIcon(icon: Component) {
-  return () => h(NIcon, null, { default: () => h(icon) })
-}
-
-const menuOptions = [
-  {
-    label: 'Hear the Wind Sing',
-    key: 'hear-the-wind-sing',
-    icon: renderIcon(BookIcon),
-  },
-  {
-    label: 'Pinball 1973',
-    key: 'pinball-1973',
-    icon: renderIcon(BookIcon),
-    disabled: true,
-    children: [
-      {
-        label: 'Rat',
-        key: 'rat',
-      },
-    ],
-  },
-  {
-    label: 'A Wild Sheep Chase',
-    key: 'a-wild-sheep-chase',
-    disabled: true,
-    icon: renderIcon(BookIcon),
-  },
-  {
-    label: 'Dance Dance Dance',
-    key: 'Dance Dance Dance',
-    icon: renderIcon(BookIcon),
-    children: [
-      {
-        type: 'group',
-        label: 'People',
-        key: 'people',
-        children: [
-          {
-            label: 'Narrator',
-            key: 'narrator',
-            icon: renderIcon(PersonIcon),
-          },
-          {
-            label: 'Sheep Man',
-            key: 'sheep-man',
-            icon: renderIcon(PersonIcon),
-          },
-        ],
-      },
-      {
-        label: 'Beverage',
-        key: 'beverage',
-        icon: renderIcon(WineIcon),
-        children: [
-          {
-            label: 'Whisky',
-            key: 'whisky',
-          },
-        ],
-      },
-      {
-        label: 'Food',
-        key: 'food',
-        children: [
-          {
-            label: 'Sandwich',
-            key: 'sandwich',
-          },
-        ],
-      },
-      {
-        label: 'The past increases. The future recedes.',
-        key: 'the-past-increases-the-future-recedes',
-      },
-    ],
-  },
-]
 </script>
 
 <style>
--- web/pose-gui/src/components/PoseViewer.vue
+++ web/pose-gui/src/components/PoseViewer.vue
@@ -2,21 +2,10 @@
 import { NLayoutContent } from 'naive-ui'
 import { TresCanvas } from '@tresjs/core'
 import SubComponent from './SubComponent.vue'
-import { state } from "@/socket";
-import { connect, disconnect, sub } from '../composables/socketManager';
-import { computed } from 'vue'
-
-
-const connected = computed(() => state.connected);
-
 </script>
 
 <template>
   <n-layout-content>
-    <p @click="connect()">connect</p>
-    <p @click="disconnect()">disconnect</p>
-    <p @click="sub()">sub</p>
-    <p>State: {{ connected }}</p>
     <TresCanvas clear-color="#82DBC5">
       <TresPerspectiveCamera
         :position="[3, 3, 3]"
