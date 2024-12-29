bbf34e8d77ff5fd57b87351e60bd13b07e2b4bf4
darrenchang <darrenchang951@gmail.com>
2024-12-29 21:37:04 +0800

    Modify cube size for better visibility

--- web/pose-gui/src/components/PoseViewer.vue
+++ web/pose-gui/src/components/PoseViewer.vue
@@ -7,43 +7,181 @@ import { shallowRef } from 'vue'
 const { onLoop } = useRenderLoop()
 
 interface poseLandmarksInterface {
-  [key: string]: number[];
+  [key: string]: {
+    cubeSize: number[]
+    cubeColor: string
+    position: number[]
+  }
 }
 const factor = 2
 let poseLandmarks: poseLandmarksInterface = {
-  nose: [0, 0, 0],
-  leftEyeInner: [0, 0, 0],
-  leftEye: [0, 0, 0],
-  leftEyeOuter: [0, 0, 0],
-  rightEyeInner: [0, 0, 0],
-  rightEye: [0, 0, 0],
-  rightEyeOuter: [0, 0, 0],
-  leftEar: [0, 0, 0],
-  rightEar: [0, 0, 0],
-  mouthLeft: [0, 0, 0],
-  mouthRight: [0, 0, 0],
-  leftShoulder: [0, 0, 0],
-  rightShoulder: [0, 0, 0],
-  leftElbow: [0, 0, 0],
-  rightElbow: [0, 0, 0],
-  leftWrist: [0, 0, 0],
-  rightWrist: [0, 0, 0],
-  leftPinky: [0, 0, 0],
-  rightPinky: [0, 0, 0],
-  leftIndex: [0, 0, 0],
-  rightIndex: [0, 0, 0],
-  leftThumb: [0, 0, 0],
-  rightThumb: [0, 0, 0],
-  leftHip: [0, 0, 0],
-  rightHip: [0, 0, 0],
-  leftKnee: [0, 0, 0],
-  rightKnee: [0, 0, 0],
-  leftAnkle: [0, 0, 0],
-  rightAnkle: [0, 0, 0],
-  leftHeel: [0, 0, 0],
-  rightHeel: [0, 0, 0],
-  leftFootIndex: [0, 0, 0],
-  rightFootIndex: [0, 0, 0],
+  // head
+  nose: {
+    cubeColor: 'orage',
+    cubeSize: [0.05, 0.05, 0.05],
+    position: [0, 0, 0],
+  },
+  leftEyeInner: {
+    cubeColor: 'orage',
+    cubeSize: [0.02, 0.02, 0.02],
+    position: [0, 0, 0],
+  },
+  leftEye: {
+    cubeColor: 'orage',
+    cubeSize: [0.02, 0.02, 0.02],
+    position: [0, 0, 0],
+  },
+  leftEyeOuter: {
+    cubeColor: 'orage',
+    cubeSize: [0.02, 0.02, 0.02],
+    position: [0, 0, 0],
+  },
+  rightEyeInner: {
+    cubeColor: 'orage',
+    cubeSize: [0.02, 0.02, 0.02],
+    position: [0, 0, 0],
+  },
+  rightEye: {
+    cubeColor: 'orage',
+    cubeSize: [0.02, 0.02, 0.02],
+    position: [0, 0, 0],
+  },
+  rightEyeOuter: {
+    cubeColor: 'orage',
+    cubeSize: [0.02, 0.02, 0.02],
+    position: [0, 0, 0],
+  },
+  leftEar: {
+    cubeColor: 'orage',
+    cubeSize: [0.05, 0.05, 0.05],
+    position: [0, 0, 0],
+  },
+  rightEar: {
+    cubeColor: 'orage',
+    cubeSize: [0.05, 0.05, 0.05],
+    position: [0, 0, 0],
+  },
+  mouthLeft: {
+    cubeColor: 'orage',
+    cubeSize: [0.05, 0.05, 0.05],
+    position: [0, 0, 0],
+  },
+  mouthRight: {
+    cubeColor: 'orage',
+    cubeSize: [0.05, 0.05, 0.05],
+    position: [0, 0, 0],
+  },
+  // body
+  leftShoulder: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  rightShoulder: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  leftElbow: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  rightElbow: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  leftWrist: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  rightWrist: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  leftPinky: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  rightPinky: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  leftIndex: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  rightIndex: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  leftThumb: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  rightThumb: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  leftHip: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  rightHip: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  leftKnee: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  rightKnee: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  leftAnkle: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  rightAnkle: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  leftHeel: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  rightHeel: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  leftFootIndex: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  rightFootIndex: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
 }
 
 socket.on('pose_landmarks', message => {
@@ -51,18 +189,22 @@ socket.on('pose_landmarks', message => {
     return
   }
   Object.keys(poseLandmarks).forEach((key, index) => {
-    const position = [message[index]['x'] * factor, message[index]['y'] * factor, message[index]['z'] * factor];
-    poseLandmarks[key] = position;
-  });
+    const position = [
+      message[index]['x'] * factor,
+      message[index]['y'] * factor,
+      message[index]['z'] * factor,
+    ]
+    poseLandmarks[key].position = position
+  })
 })
 
 function lerp(start: number, end: number, delta: number) {
   const smoothingFactor = 10
-  const threshold = 0.3;
+  const threshold = 0.3
   if (Math.abs(end - start) > threshold) {
-    return end;
+    return end
   } else {
-    return start + (end - start) * delta * smoothingFactor;
+    return start + (end - start) * delta * smoothingFactor
   }
 }
 
@@ -71,14 +213,14 @@ onLoop(({ delta, elapsed }) => {
   if (!landmarksGroupRef.value) {
     return
   }
-  const landmarks: any[] = landmarksGroupRef.value.children;
+  const landmarks: any[] = landmarksGroupRef.value.children
   landmarks.forEach((item, _) => {
-    const newX = 1 - poseLandmarks[item.name][0];
-    item.position.x = lerp(item.position.x, newX, delta);
-    const newY = 1 - poseLandmarks[item.name][1];
-    item.position.y = lerp(item.position.y, newY, delta);
-    const newZ = 1 - poseLandmarks[item.name][2];
-    item.position.z = lerp(item.position.z, newZ, delta);
+    const newX = 1 - poseLandmarks[item.name].position[0]
+    item.position.x = lerp(item.position.x, newX, delta)
+    const newY = 1 - poseLandmarks[item.name].position[1]
+    item.position.y = lerp(item.position.y, newY, delta)
+    const newZ = 1 - poseLandmarks[item.name].position[2]
+    item.position.z = lerp(item.position.z, newZ, delta)
   })
 })
 </script>
@@ -87,10 +229,10 @@ onLoop(({ delta, elapsed }) => {
   <n-layout-content>
     <TresCanvas clear-color="#82DBC5">
       <TresPerspectiveCamera :position="[0, 0, 6]" :fov="45" :look-at="[0, 0, 0]" />
-      <TresGroup ref="landmarksGroupRef" :position="[0,0,0]">
+      <TresGroup ref="landmarksGroupRef" :position="[0, 0, 0]">
         <TresMesh v-for="(landmark, key) in poseLandmarks" :name="key" :key="key" :position="[-1, -1, -1]">
-          <TresBoxGeometry :args="[0.1, 0.1, 0.1]" />
-          <TresMeshNormalMaterial color="orange" />
+          <TresBoxGeometry :args="landmark.cubeSize" />
+          <TresMeshNormalMaterial :color="landmark.cubeColor" />
         </TresMesh>
       </TresGroup>
       <TresAmbientLight :intensity="1" />
