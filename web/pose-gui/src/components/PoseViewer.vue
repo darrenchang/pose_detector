96e2471f1517b26a6c3a82f758477e168b47d534
darrenchang <darrenchang951@gmail.com>
2025-01-18 21:17:58 +0800

    Refactor

--- web/pose-gui/src/components/PoseViewer.vue
+++ web/pose-gui/src/components/PoseViewer.vue
@@ -3,186 +3,12 @@ import { NLayoutContent } from 'naive-ui'
 import { TresCanvas, useRenderLoop } from '@tresjs/core'
 import { socket } from '@/socket'
 import { shallowRef } from 'vue'
+import { examplePoseLandmarks } from '@/composables/poseLandmarksInterface'
 
 const { onLoop } = useRenderLoop()
 
-interface poseLandmarksInterface {
-  [key: string]: {
-    cubeSize: number[]
-    cubeColor: string
-    position: number[]
-  }
-}
 const factor = 2
-let poseLandmarks: poseLandmarksInterface = {
-  // head
-  nose: {
-    cubeColor: 'orage',
-    cubeSize: [0.05, 0.05, 0.05],
-    position: [0, 0, 0],
-  },
-  leftEyeInner: {
-    cubeColor: 'orage',
-    cubeSize: [0.02, 0.02, 0.02],
-    position: [0, 0, 0],
-  },
-  leftEye: {
-    cubeColor: 'orage',
-    cubeSize: [0.02, 0.02, 0.02],
-    position: [0, 0, 0],
-  },
-  leftEyeOuter: {
-    cubeColor: 'orage',
-    cubeSize: [0.02, 0.02, 0.02],
-    position: [0, 0, 0],
-  },
-  rightEyeInner: {
-    cubeColor: 'orage',
-    cubeSize: [0.02, 0.02, 0.02],
-    position: [0, 0, 0],
-  },
-  rightEye: {
-    cubeColor: 'orage',
-    cubeSize: [0.02, 0.02, 0.02],
-    position: [0, 0, 0],
-  },
-  rightEyeOuter: {
-    cubeColor: 'orage',
-    cubeSize: [0.02, 0.02, 0.02],
-    position: [0, 0, 0],
-  },
-  leftEar: {
-    cubeColor: 'orage',
-    cubeSize: [0.05, 0.05, 0.05],
-    position: [0, 0, 0],
-  },
-  rightEar: {
-    cubeColor: 'orage',
-    cubeSize: [0.05, 0.05, 0.05],
-    position: [0, 0, 0],
-  },
-  mouthLeft: {
-    cubeColor: 'orage',
-    cubeSize: [0.05, 0.05, 0.05],
-    position: [0, 0, 0],
-  },
-  mouthRight: {
-    cubeColor: 'orage',
-    cubeSize: [0.05, 0.05, 0.05],
-    position: [0, 0, 0],
-  },
-  // body
-  leftShoulder: {
-    cubeColor: 'orage',
-    cubeSize: [0.1, 0.1, 0.1],
-    position: [0, 0, 0],
-  },
-  rightShoulder: {
-    cubeColor: 'orage',
-    cubeSize: [0.1, 0.1, 0.1],
-    position: [0, 0, 0],
-  },
-  leftElbow: {
-    cubeColor: 'orage',
-    cubeSize: [0.1, 0.1, 0.1],
-    position: [0, 0, 0],
-  },
-  rightElbow: {
-    cubeColor: 'orage',
-    cubeSize: [0.1, 0.1, 0.1],
-    position: [0, 0, 0],
-  },
-  leftWrist: {
-    cubeColor: 'orage',
-    cubeSize: [0.05, 0.05, 0.05],
-    position: [0, 0, 0],
-  },
-  rightWrist: {
-    cubeColor: 'orage',
-    cubeSize: [0.05, 0.05, 0.05],
-    position: [0, 0, 0],
-  },
-  leftPinky: {
-    cubeColor: 'orage',
-    cubeSize: [0.05, 0.05, 0.05],
-    position: [0, 0, 0],
-  },
-  rightPinky: {
-    cubeColor: 'orage',
-    cubeSize: [0.05, 0.05, 0.05],
-    position: [0, 0, 0],
-  },
-  leftIndex: {
-    cubeColor: 'orage',
-    cubeSize: [0.05, 0.05, 0.05],
-    position: [0, 0, 0],
-  },
-  rightIndex: {
-    cubeColor: 'orage',
-    cubeSize: [0.05, 0.05, 0.05],
-    position: [0, 0, 0],
-  },
-  leftThumb: {
-    cubeColor: 'orage',
-    cubeSize: [0.05, 0.05, 0.05],
-    position: [0, 0, 0],
-  },
-  rightThumb: {
-    cubeColor: 'orage',
-    cubeSize: [0.05, 0.05, 0.05],
-    position: [0, 0, 0],
-  },
-  leftHip: {
-    cubeColor: 'orage',
-    cubeSize: [0.1, 0.1, 0.1],
-    position: [0, 0, 0],
-  },
-  rightHip: {
-    cubeColor: 'orage',
-    cubeSize: [0.1, 0.1, 0.1],
-    position: [0, 0, 0],
-  },
-  leftKnee: {
-    cubeColor: 'orage',
-    cubeSize: [0.1, 0.1, 0.1],
-    position: [0, 0, 0],
-  },
-  rightKnee: {
-    cubeColor: 'orage',
-    cubeSize: [0.1, 0.1, 0.1],
-    position: [0, 0, 0],
-  },
-  leftAnkle: {
-    cubeColor: 'orage',
-    cubeSize: [0.1, 0.1, 0.1],
-    position: [0, 0, 0],
-  },
-  rightAnkle: {
-    cubeColor: 'orage',
-    cubeSize: [0.1, 0.1, 0.1],
-    position: [0, 0, 0],
-  },
-  leftHeel: {
-    cubeColor: 'orage',
-    cubeSize: [0.1, 0.1, 0.1],
-    position: [0, 0, 0],
-  },
-  rightHeel: {
-    cubeColor: 'orage',
-    cubeSize: [0.1, 0.1, 0.1],
-    position: [0, 0, 0],
-  },
-  leftFootIndex: {
-    cubeColor: 'orage',
-    cubeSize: [0.1, 0.1, 0.1],
-    position: [0, 0, 0],
-  },
-  rightFootIndex: {
-    cubeColor: 'orage',
-    cubeSize: [0.1, 0.1, 0.1],
-    position: [0, 0, 0],
-  },
-}
+const poseLandmarks = examplePoseLandmarks
 
 socket.on('pose_landmarks', message => {
   if (message.pose_landmarks.length <= 0) {
@@ -229,18 +55,9 @@ onLoop(({ delta, elapsed }) => {
 <template>
   <n-layout-content>
     <TresCanvas clear-color="#82DBC5">
-      <TresPerspectiveCamera
-        :position="[0, 0, 6]"
-        :fov="45"
-        :look-at="[0, 0, 0]"
-      />
+      <TresPerspectiveCamera :position="[0, 0, 6]" :fov="45" :look-at="[0, 0, 0]" />
       <TresGroup ref="landmarksGroupRef" :position="[0, 0, 0]">
-        <TresMesh
-          v-for="(landmark, key) in poseLandmarks"
-          :name="key"
-          :key="key"
-          :position="[-1, -1, -1]"
-        >
+        <TresMesh v-for="(landmark, key) in poseLandmarks" :name="key" :key="key" :position="[-1, -1, -1]">
           <TresBoxGeometry :args="landmark.cubeSize" />
           <TresMeshNormalMaterial :color="landmark.cubeColor" />
         </TresMesh>
---
+++ web/pose-gui/src/composables/poseLandmarksInterface.ts
@@ -0,0 +1,177 @@
+export interface poseLandmarksInterface {
+  [key: string]: {
+    cubeSize: number[]
+    cubeColor: string
+    position: number[]
+  }
+}
+
+export const examplePoseLandmarks: poseLandmarksInterface = {
+  // head
+  nose: {
+    cubeColor: 'orage',
+    cubeSize: [0.05, 0.05, 0.05],
+    position: [0, 0, 0],
+  },
+  leftEyeInner: {
+    cubeColor: 'orage',
+    cubeSize: [0.02, 0.02, 0.02],
+    position: [0, 0, 0],
+  },
+  leftEye: {
+    cubeColor: 'orage',
+    cubeSize: [0.02, 0.02, 0.02],
+    position: [0, 0, 0],
+  },
+  leftEyeOuter: {
+    cubeColor: 'orage',
+    cubeSize: [0.02, 0.02, 0.02],
+    position: [0, 0, 0],
+  },
+  rightEyeInner: {
+    cubeColor: 'orage',
+    cubeSize: [0.02, 0.02, 0.02],
+    position: [0, 0, 0],
+  },
+  rightEye: {
+    cubeColor: 'orage',
+    cubeSize: [0.02, 0.02, 0.02],
+    position: [0, 0, 0],
+  },
+  rightEyeOuter: {
+    cubeColor: 'orage',
+    cubeSize: [0.02, 0.02, 0.02],
+    position: [0, 0, 0],
+  },
+  leftEar: {
+    cubeColor: 'orage',
+    cubeSize: [0.05, 0.05, 0.05],
+    position: [0, 0, 0],
+  },
+  rightEar: {
+    cubeColor: 'orage',
+    cubeSize: [0.05, 0.05, 0.05],
+    position: [0, 0, 0],
+  },
+  mouthLeft: {
+    cubeColor: 'orage',
+    cubeSize: [0.05, 0.05, 0.05],
+    position: [0, 0, 0],
+  },
+  mouthRight: {
+    cubeColor: 'orage',
+    cubeSize: [0.05, 0.05, 0.05],
+    position: [0, 0, 0],
+  },
+  // body
+  leftShoulder: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  rightShoulder: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  leftElbow: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  rightElbow: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  leftWrist: {
+    cubeColor: 'orage',
+    cubeSize: [0.05, 0.05, 0.05],
+    position: [0, 0, 0],
+  },
+  rightWrist: {
+    cubeColor: 'orage',
+    cubeSize: [0.05, 0.05, 0.05],
+    position: [0, 0, 0],
+  },
+  leftPinky: {
+    cubeColor: 'orage',
+    cubeSize: [0.05, 0.05, 0.05],
+    position: [0, 0, 0],
+  },
+  rightPinky: {
+    cubeColor: 'orage',
+    cubeSize: [0.05, 0.05, 0.05],
+    position: [0, 0, 0],
+  },
+  leftIndex: {
+    cubeColor: 'orage',
+    cubeSize: [0.05, 0.05, 0.05],
+    position: [0, 0, 0],
+  },
+  rightIndex: {
+    cubeColor: 'orage',
+    cubeSize: [0.05, 0.05, 0.05],
+    position: [0, 0, 0],
+  },
+  leftThumb: {
+    cubeColor: 'orage',
+    cubeSize: [0.05, 0.05, 0.05],
+    position: [0, 0, 0],
+  },
+  rightThumb: {
+    cubeColor: 'orage',
+    cubeSize: [0.05, 0.05, 0.05],
+    position: [0, 0, 0],
+  },
+  leftHip: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  rightHip: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  leftKnee: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  rightKnee: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  leftAnkle: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  rightAnkle: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  leftHeel: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  rightHeel: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  leftFootIndex: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+  rightFootIndex: {
+    cubeColor: 'orage',
+    cubeSize: [0.1, 0.1, 0.1],
+    position: [0, 0, 0],
+  },
+}
