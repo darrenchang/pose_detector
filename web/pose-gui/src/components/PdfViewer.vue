ef6c409bafe073ddf8af55b973d2bc687a0ae495
darrenchang <darrenchang951@gmail.com>
2025-03-25 17:34:03 +0800

    Add canvas for wrapper for pdf viewer

--- web/pose-gui/src/components/PdfViewer.vue
+++ web/pose-gui/src/components/PdfViewer.vue
@@ -1,24 +1,33 @@
 <script setup lang="ts">
 import { NLayoutContent } from 'naive-ui'
 import { onMounted } from 'vue'
+import { ref } from 'vue'
 import { pdfjsLib } from '@/composables/pdfjsLib'
 
 const pdfSrc = 'https://pdfobject.com/pdf/pdf_open_parameters_acro8.pdf'
+let numPages = ref(0)
+let currentPage = ref(1)
+let pdfProxy = undefined
+let pdf = undefined
 
 function processLoadingTask(source: string) {
-  console.log('processLoadingTask')
   const loadingTask = pdfjsLib.getDocument(source)
   loadingTask.promise
-    .then(docProxy => {
-      console.log(docProxy)
+    .then(doc => {
+      pdfProxy = doc
+      pdf = doc.loadingTask
+      numPages.value = doc.numPages
+      console.log("pdfProxy", pdfProxy)
+      console.log("pdf", pdf)
+      return doc.getPage(currentPage.value)
     })
     .then(page => {
       const renderContext = {
         // canvasContext: context,
         // viewport: viewport,
       }
-      console.log("loaded")
-      console.log(page)
+      console.log('page', page)
+      console.log('renderContext', renderContext)
       // return page.render(renderContext)
     })
 }
@@ -32,8 +41,25 @@ onMounted(async () => {
 
 <template>
   <n-layout-content>
-    <div>hello</div>
+    <div>Pages: {{numPages}}, Current Page: {{currentPage}}</div>
+    <div
+      ref="pdfLayersWrapper"
+      class="pdf__layers pdfLayers"
+      style="height: 300px;"
+    >
+      <div class="pdf__canvas-layer">
+        <canvas ref="canvasLayer" />
+      </div>
+      <div ref="textLayer" class="pdf__text-layer"></div>
+      <div ref="annotationLayer" class="pdf__annotation-layer"></div>
+    </div>
   </n-layout-content>
 </template>
 
-<style scoped></style>
+<style scoped>
+.pdfLayers {
+  background-color: red;
+  border: 0;
+  margin: 0;
+}
+</style>
