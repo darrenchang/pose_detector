8298c99737ad0724639d883e06e0a1d78c54603b
darrenchang <darrenchang951@gmail.com>
2025-03-25 13:42:54 +0800

    Disconnect socketio on unMounted

--- web/pose-gui/src/views/PoseView.vue
+++ web/pose-gui/src/views/PoseView.vue
@@ -22,13 +22,13 @@
 
 <script setup lang="ts">
 import PoseViewer from '@/components/PoseViewer.vue'
-import { onMounted } from 'vue'
+import { onMounted, onUnmounted } from 'vue'
 import type { Component } from 'vue'
 import { NIcon } from 'naive-ui'
 import { h } from 'vue'
 import { NLayout, NLayoutSider, NLayoutContent, NMenu } from 'naive-ui'
 import { VideocamOutline } from '@vicons/ionicons5'
-import { connect, sub } from '@/composables/socketManager'
+import { connect, sub, disconnect } from '@/composables/socketManager'
 
 function renderIcon(icon: Component) {
   return () => h(NIcon, null, { default: () => h(icon) })
@@ -46,6 +46,10 @@ const menuOptionsLandmarkSource = [
 onMounted(async () => {
   connect();
   sub(defaultLandmarkSource);
+})
+
+onUnmounted(async () => {
+  disconnect();
 })
 
 function handleUpdateKeys(key: string) {
