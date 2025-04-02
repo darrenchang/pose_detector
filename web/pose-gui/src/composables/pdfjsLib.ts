d7fb5e7d3c9e329e1fd5c983c46660f2e875715c
darrenchang <darrenchang951@gmail.com>
2025-04-02 15:15:46 +0800

    Include pdf_viewer.css style for pdfjs

--- web/pose-gui/src/composables/pdfjsLib.ts
+++ web/pose-gui/src/composables/pdfjsLib.ts
@@ -1,5 +1,6 @@
 import * as PDFJS from "pdfjs-dist";
 import * as PDFWorker from "pdfjs-dist/build/pdf.worker.min";
+import 'pdfjs-dist/web/pdf_viewer.css'
 
 export const pdfjsLib = PDFJS;
 export const pdfWorkerLib = PDFWorker;
